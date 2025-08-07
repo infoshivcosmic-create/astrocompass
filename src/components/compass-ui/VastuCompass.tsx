763870de88c3446a4c4d97470d896835c89bd456
shiv cosmic <info.shivcosmic@gmail.com>
2025-08-07 17:41:38 +0000

    remove this red line

--- src/components/compass-ui/VastuCompass.tsx
+++ src/components/compass-ui/VastuCompass.tsx
@@ -11,13 +11,6 @@ interface VastuCompassProps {
 const VastuCompass: React.FC<VastuCompassProps> = ({ rotation }) => {
   return (
     <div className="relative w-80 h-80 sm:w-96 sm:h-96 mx-auto my-8">
-       {/* Red needle pointing up */}
-       <div className="absolute top-0 left-1/2 -translate-x-1/2 z-10" style={{'height': '50%'}}>
-        <svg width="20" height="100%" viewBox="0 0 20 160">
-            <polygon points="10,0 20,20 10,160 0,20" fill="#E53E3E"/>
-        </svg>
-      </div>
-
       <div
         className="w-full h-full transition-transform duration-500 ease-in-out"
         style={{ transform: `rotate(${rotation}deg)` }}
