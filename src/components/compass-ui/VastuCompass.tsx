6684bfe545c38f3d8a0f14c01c80d0cf9062ccfc
shiv cosmic <info.shivcosmic@gmail.com>
2025-08-07 17:43:55 +0000

    Add the upward triangle here in red color. the tringle upward point will

--- src/components/compass-ui/VastuCompass.tsx
+++ src/components/compass-ui/VastuCompass.tsx
@@ -11,6 +11,11 @@ interface VastuCompassProps {
 const VastuCompass: React.FC<VastuCompassProps> = ({ rotation }) => {
   return (
     <div className="relative w-80 h-80 sm:w-96 sm:h-96 mx-auto my-8">
+      <div className="absolute -top-2 left-1/2 -translate-x-1/2 z-10">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 0L24 21H0L12 0Z" fill="#FF0000"/>
+        </svg>
+      </div>
       <div
         className="w-full h-full transition-transform duration-500 ease-in-out"
         style={{ transform: `rotate(${rotation}deg)` }}
