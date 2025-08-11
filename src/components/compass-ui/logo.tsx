a64090d0dca715e12b6e941d72fef1d85d2a7245
shiv cosmic <info.shivcosmic@gmail.com>
2025-08-11 11:51:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/compass-ui/logo.tsx
+++ src/components/compass-ui/logo.tsx
@@ -4,7 +4,7 @@ import type {HTMLAttributes} from 'react';
 export function Logo(props: HTMLAttributes<HTMLDivElement>) {
   return (
     <div {...props}>
-      <Image src="/images/logo.png" alt="Shiv Cosmic Energy Solutions" width={60} height={20} />
+      <Image src="/images/logo.png" alt="Shiv Cosmic Energy Solutions" width={60} height={20} unoptimized />
     </div>
   );
 }
