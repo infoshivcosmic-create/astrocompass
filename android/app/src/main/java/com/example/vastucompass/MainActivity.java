dd71727c5a352e41005ad07a650cb36492731a73
shiv cosmic <info.shivcosmic@gmail.com>
2025-08-11 10:07:04 +0000

    Initial commit

---
+++ android/app/src/main/java/com/example/vastucompass/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.vastucompass;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.vastucompass',
+  appName: 'Vastu Compass',
+  webDir: 'out'
+};
+
+export default config;
